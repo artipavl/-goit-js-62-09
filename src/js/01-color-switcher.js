3c84ce1a86c9677e791889e8a3609c15ffb09d25
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-11 15:12:51 +0300

    Додає виконання 1 ДЗ

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const btnStart = document.querySelector("[data-start]");
+const btnStop = document.querySelector("[data-stop]");
 
+let timerId = null;
+let changeBogyColor = false;
+
+btnStart.addEventListener('click', onBodyColorStart);
+btnStop.addEventListener('click', onBodyColorStop);
+
+offbtn(btnStop);
+
+function onBodyColorStart(e) {
+    if (changeBogyColor) {
+        return;
+    }
+    
+    offbtn(e.currentTarget);
+    onbtn(btnStop);
+    changeBogyColor = true;
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    },1000);
+}
+
+function onBodyColorStop() {
+    clearInterval(timerId);
+    changeBogyColor = false;
+    onbtn(btnStart);
+    offbtn(btnStop);
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function offbtn(btn) {
+    btn.setAttribute("disabled", "disabled");
+}
+
+function onbtn(btn) {
+    btn.removeAttribute("disabled");
+}
